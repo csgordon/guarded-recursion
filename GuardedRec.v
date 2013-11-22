158a3eeab7eccc1c592eef49eaeb60f67e455fd3
Colin Stebbins Gordon <csgordon@cs.washington.edu>
2013-11-22 15:12:59 -0800

    Adopt the definitional approach for Coq

--- GuardedRec.v
+++ GuardedRec.v
@@ -4,23 +4,31 @@ Require Import Utf8.
 
 (** ** Syntactic Motivation *)
 (** *** II.A: Fixed Points on Universes *)
-Axiom later : Type -> Type.
+Inductive later (T: Type) : Type :=
+  next : T -> later T.
+Arguments next [T] _.
 Notation "▸ T" := (later T) (at level 50). (* \blacktriangleright *)
-Axiom next : forall {T:Type}, T -> ▸ T.
-Axiom lapp : forall {A B:Type}, ▸(A->B) -> ▸A -> ▸B.
+Definition lapp {A B:Type} (t : ▸(A->B)) (u : ▸A) : ▸B :=
+  match t,u with
+  | next t', next u' => next (t' u')
+  end.
 Notation "t ⊛ u" := (lapp t u) (at level 49). (* \circledast *)
 
-(* "Definitional" equalities *)
+(** Verify the paper's definitional equalities *)
 Definition nd_compose {A B C:Type} (f:B->C) (g:A->B) : A -> C :=
   λ a, f (g a).
 Notation "f ∘ g" := (nd_compose f g) (at level 55).
-Axiom next_id : forall (T:Type) u, next (λ (x:T), x) ⊛ u = u.
-Axiom next_compose : forall (A B C:Type) s t u,
-                       next(@nd_compose A B C) ⊛ s ⊛ t ⊛ u = s ⊛ (t ⊛ u).
-Axiom next_app : forall (A B :Type) (t:A->B) u,
+Lemma next_id : forall (T:Type) u, next (λ (x:T), x) ⊛ u = u.
+Proof. intros. destruct u. simpl. reflexivity. Qed.
+Lemma next_compose : forall (A B C:Type) s t u,
+                       next (@nd_compose A B C) ⊛ s ⊛ t ⊛ u = s ⊛ (t ⊛ u).
+Proof. intros. destruct s; destruct t; destruct u. simpl. unfold nd_compose. reflexivity. Qed.
+Lemma next_app : forall (A B :Type) (t:A->B) u,
                    next t ⊛ next u = next (t u).
-Axiom next_right : forall (A B:Type) u (t:A),
+Proof. intros. simpl. reflexivity. Qed.
+Lemma next_right : forall (A B:Type) u (t:A),
                      u ⊛ next t = next (λ (g:A->B), g t) ⊛ u.
+Proof. intros. destruct u. simpl. reflexivity. Qed.
 
 (* Fixpoints *)
 Axiom gfix : forall {A:Type}, ((▸A)->A) -> A.
@@ -46,9 +54,6 @@ Lemma pfix_red : forall {A B:Type} t,
                    @pfix A B t = t (λ y, next (pfix t y)).
 Proof. intros. unfold pfix.
        rewrite gfix_red at 1. f_equal.
-       Require Import FunctionalExtensionality.
-       apply FunctionalExtensionality.functional_extensionality.
-       intros. rewrite next_app. reflexivity.
 Qed.
 
 Require Import Coq.Vectors.Fin.
